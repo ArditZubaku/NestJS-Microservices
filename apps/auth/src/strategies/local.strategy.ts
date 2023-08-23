a33c0e37dcc3d437038dc6e846deb74567edc84c
Ardit Zubaku <zubaku92@gmail.com>
2023-08-23 11:48:27 +0200

    CurrentUser decorator.

--- apps/auth/src/strategies/local.strategy.ts
+++ apps/auth/src/strategies/local.strategy.ts
@@ -21,6 +21,7 @@ export class LocalStrategy extends PassportStrategy(
   // Gets called by the extended PassportStrategy
   validate(email: string, password: string): Promise<UsersDocument> {
     try {
+      // Whatever gets validated gets automatically added to the request object
       return this.usersService.verifyUser(email, password);
     } catch (error) {
       throw new UnauthorizedException(error);
