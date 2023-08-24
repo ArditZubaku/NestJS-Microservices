478a49ce6a4951a07a2a6030e2877667248e5b78
Ardit Zubaku <zubaku92@gmail.com>
2023-08-24 13:42:57 +0200

    Applying validation to the incoming data.

--- apps/payments/src/payments.controller.ts
+++ apps/payments/src/payments.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UsePipes, ValidationPipe } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { CreateChargeDto } from '@app/common';
@@ -9,6 +9,7 @@ export class PaymentsController {
 
   // Exposing the microservice functionality to other microservices
   @MessagePattern('create_charge')
+  @UsePipes(new ValidationPipe()) // Will automatically validate all the incoming data on this incoming payload => will validate using CreateChargeDto
   async createCharge(@Payload() data: CreateChargeDto) {
     return this.paymentsService.createCharge(data);
   }
