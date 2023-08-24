e57cf03c51dc03cb402741135b0aa8085ee85359
Ardit Zubaku <zubaku92@gmail.com>
2023-08-24 15:56:24 +0200

    Nodemailer and Google OAuth2 setup.

--- apps/notifications/src/notifications.service.ts
+++ apps/notifications/src/notifications.service.ts
@@ -1,9 +1,29 @@
+import { ConfigService } from '@nestjs/config';
 import { Injectable } from '@nestjs/common';
 import { NotifyEmailDto } from './dto/notify-email.dto';
+import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class NotificationsService {
-  async notifyEmail(email: NotifyEmailDto) {
-    console.log(email);
+  constructor(private readonly configService: ConfigService) {}
+
+  private readonly mailer = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: this.configService.get('SMTP_USER'),
+      clientId: this.configService.get('GOOGLE_OAUTH_CLIENT_ID'),
+      clientSecret: this.configService.get('GOOGLE_OAUTH_CLIENT_SECRET'),
+      refreshToken: this.configService.get('GOOGLE_OAUTH_REFRESH_TOKEN'),
+    },
+  });
+
+  async notifyEmail({ email }: NotifyEmailDto) {
+    await this.mailer.sendMail({
+      from: this.configService.get('SMTP_USER'),
+      to: email,
+      subject: 'NestJS Microservice - Notifications',
+      text: 'Test text',
+    });
   }
 }
