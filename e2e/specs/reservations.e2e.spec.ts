83d4bb3771ebcdfb2fcf3936f54a71d4d5bc4f9b
ArditZubaku <zubaku92@gmail.com>
2023-09-29 16:43:55 +0200

    Create & Get reservation test.

--- e2e/specs/reservations.e2e.spec.ts
+++ e2e/specs/reservations.e2e.spec.ts
@@ -23,31 +23,51 @@ describe('Reservations', (): void => {
   });
 
   test('Create & Get', async () => {
-    const res: Response = await fetch('http://reservations:3021/reservations', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authentication: jwt,
-      },
-      body: JSON.stringify({
-        startDate: '12-12/2023',
-        endDate: '12/24/2024',
-        placeId: '12345',
-        invoiceId: '499',
-        charge: {
-          amount: 5,
-          card: {
-            cvc: '413',
-            exp_month: 12,
-            exp_year: 2030,
-            number: '4242 4242 4242 4242',
-          },
-        },
-      }),
-    });
-    expect(res.ok).toBeTruthy();
+    const createdReservation = await createReservation();
 
-    const reservation = await res.json();
-    console.log(reservation);
+    const resGet: Response = await fetch(
+      `http://reservations:3021/reservations/${createdReservation._id}`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authentication: jwt,
+        },
+      },
+    );
+
+    const reservation = await resGet.json();
+
+    expect(createdReservation).toEqual(reservation);
   });
+
+  const createReservation = async () => {
+    const resCreate: Response = await fetch(
+      'http://reservations:3021/reservations',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authentication: jwt,
+        },
+        body: JSON.stringify({
+          startDate: '12-12/2023',
+          endDate: '12/24/2024',
+          placeId: '12345',
+          invoiceId: '499',
+          charge: {
+            amount: 5,
+            card: {
+              cvc: '413',
+              exp_month: 12,
+              exp_year: 2030,
+              number: '4242 4242 4242 4242',
+            },
+          },
+        }),
+      },
+    );
+    expect(resCreate.ok).toBeTruthy();
+
+    return resCreate.json();
+  };
 });
