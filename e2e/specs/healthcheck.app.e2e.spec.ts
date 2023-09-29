0b42552fbcc059e5b9931bf5bd8756103e0b346c
ArditZubaku <zubaku92@gmail.com>
2023-09-29 12:06:57 +0200

    Health check on payments and notifications.

--- e2e/specs/healthcheck.app.e2e.spec.ts
+++ e2e/specs/healthcheck.app.e2e.spec.ts
@@ -1,3 +1,5 @@
+import { ping } from 'tcp-ping';
+
 describe('Health', () => {
   test('Reservation', async () => {
     const res = await fetch('http://reservations:3021');
@@ -7,12 +9,31 @@ describe('Health', () => {
     const res = await fetch('http://auth:3022');
     expect(res.ok).toBeTruthy();
   });
-  //   test('Payments', async () => {
-  //     const res = await fetch('http://payments:3021');
-  //     expect(res.ok).toBeTruthy();
-  //   });
-  //   test('Notifications', async () => {
-  //     const res = await fetch('http://notifications:3021');
-  //     expect(res.ok).toBeTruthy();
-  //   });
+
+  // When calling done, we will signify that the test was successful
+  test('Payments', (done) => {
+    ping(
+      {
+        address: 'payments',
+        port: 3024,
+      },
+      (error) => {
+        if (error) fail();
+        done();
+      },
+    );
+  });
+
+  test('Notifications', (done) => {
+    ping(
+      {
+        address: 'notifications',
+        port: 3025,
+      },
+      (error) => {
+        if (error) fail();
+        done();
+      },
+    );
+  });
 });
