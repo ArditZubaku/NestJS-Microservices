23b9a866d1ef30faaf4f025c0521dc55198a839e
ArditZubaku <zubaku92@gmail.com>
2023-10-05 14:49:14 +0200

    Manual ack.

--- apps/payments/src/main.ts
+++ apps/payments/src/main.ts
@@ -10,10 +10,13 @@ async function bootstrap() {
   const configService: ConfigService = app.get(ConfigService);
 
   app.connectMicroservice({
+    // RabbitMQ runs asynchronously so it doesn't overload like the TCP connection
     transport: Transport.RMQ,
     options: {
       urls: [configService.getOrThrow('RABBITMQ_URI')],
       queue: 'payments',
+      // Retries every time, bc the broker never gets the acknowledgement
+      noAck: false,
     },
   });
 
--- apps/payments/src/payments.controller.ts
+++ apps/payments/src/payments.controller.ts
@@ -1,6 +1,11 @@
 import { Controller, UsePipes, ValidationPipe } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import {
+  Ctx,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
 import { PaymentsCreateChargeDto } from './dto/payments-create-charge.dto';
 
 @Controller()
@@ -12,7 +17,17 @@ export class PaymentsController {
   // When a microservice receives a message that matches the specified pattern, the associated method will be invoked to handle the message.
   @MessagePattern('create_charge')
   @UsePipes(new ValidationPipe()) // Will automatically validate all the incoming data on this incoming payload => will validate using CreateChargeDto
-  async createCharge(@Payload() data: PaymentsCreateChargeDto) {
+  async createCharge(
+    @Payload() data: PaymentsCreateChargeDto,
+    @Ctx() ctx: RmqContext,
+  ) {
+    // Getting the ack manually
+    const channel = ctx.getChannelRef();
+    const originalMessage = ctx.getMessage();
+
+    // It won't retry bc we are acking the message, it takes it off the queue
+    channel.ack(originalMessage);
+
     return this.paymentsService.createCharge(data);
   }
 }
