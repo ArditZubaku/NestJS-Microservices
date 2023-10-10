467826dcb633bcb8fde04cdf90bb7cf7c7bb37cd
ArditZubaku <zubaku92@gmail.com>
2023-10-10 15:56:04 +0200

    Refactored code to use gRPC for notifications.

--- apps/notifications/src/main.ts
+++ apps/notifications/src/main.ts
@@ -4,16 +4,19 @@ import { INestApplication } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Transport } from '@nestjs/microservices';
 import { Logger } from 'nestjs-pino';
+import { NOTIFICATIONS_PACKAGE_NAME } from '@app/common';
+import { join } from 'path';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app: INestApplication = await NestFactory.create(NotificationsModule);
   const configService: ConfigService = app.get(ConfigService);
 
   app.connectMicroservice({
-    transport: Transport.RMQ,
+    transport: Transport.GRPC,
     options: {
-      urls: [configService.getOrThrow('RABBITMQ_URI')],
-      queue: 'notifications',
+      package: NOTIFICATIONS_PACKAGE_NAME,
+      protoPath: join(__dirname, '../../../proto/notifications.proto'),
+      url: configService.getOrThrow<string>('NOTIFICATIONS_GRPC_URL'),
     },
   });
 
--- apps/notifications/src/notifications.controller.ts
+++ apps/notifications/src/notifications.controller.ts
@@ -1,16 +1,19 @@
 import { Controller, UsePipes, ValidationPipe } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
-import { EventPattern, Payload } from '@nestjs/microservices';
+import { Payload } from '@nestjs/microservices';
 import { NotifyEmailDto } from './dto/notify-email.dto';
+import {
+  NotificationsServiceController,
+  NotificationsServiceControllerMethods,
+} from '@app/common';
 
 @Controller()
-export class NotificationsController {
+@NotificationsServiceControllerMethods()
+export class NotificationsController implements NotificationsServiceController {
   constructor(private readonly notificationsService: NotificationsService) {}
 
-  // Event pattern -> event based microservice communication approach where we don't send back a response from this notification service. We will simply receieve an event from a different service and do something with that without having to reply back.
   @UsePipes(new ValidationPipe()) // Applying validation based on DTOs
-  @EventPattern('notify_email')
-  async notifyEmail(@Payload() data: NotifyEmailDto) {
-    this.notificationsService.notifyEmail(data);
+  async notifyEmail(data: NotifyEmailDto): Promise<void> {
+    await this.notificationsService.notifyEmail(data);
   }
 }
