686e0e9ff5896236047c568a7d1e33ebc65211ad
ArditZubaku <zubaku92@gmail.com>
2023-10-10 15:46:59 +0200

    Refactored main.ts and Dockerfile for payments app.

--- apps/payments/src/main.ts
+++ apps/payments/src/main.ts
@@ -4,19 +4,19 @@ import { INestApplication } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Transport } from '@nestjs/microservices';
 import { Logger } from 'nestjs-pino';
+import { PAYMENTS_PACKAGE_NAME } from '@app/common';
+import { join } from 'path';
 
 async function bootstrap() {
   const app: INestApplication = await NestFactory.create(PaymentsModule);
   const configService: ConfigService = app.get(ConfigService);
 
   app.connectMicroservice({
-    // RabbitMQ runs asynchronously so it doesn't overload like the TCP connection
-    transport: Transport.RMQ,
+    transport: Transport.GRPC,
     options: {
-      urls: [configService.getOrThrow('RABBITMQ_URI')],
-      queue: 'payments',
-      // Retries every time, bc the broker never gets the acknowledgement
-      noAck: false,
+      package: PAYMENTS_PACKAGE_NAME,
+      protoPath: join(__dirname, '../../../proto/payments.proto'),
+      url: configService.getOrThrow('PAYMENTS_GRPC_URL'),
     },
   });
 
