fbe247dc00e9f53617a0aa2fc4a3591ee5940147
ArditZubaku <zubaku92@gmail.com>
2023-10-10 15:42:22 +0200

    Refactored payments controller to use the provided decorator by ts proto.

--- apps/payments/src/payments.controller.ts
+++ apps/payments/src/payments.controller.ts
@@ -1,33 +1,20 @@
 import { Controller, UsePipes, ValidationPipe } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
-import {
-  Ctx,
-  MessagePattern,
-  Payload,
-  RmqContext,
-} from '@nestjs/microservices';
 import { PaymentsCreateChargeDto } from './dto/payments-create-charge.dto';
+import {
+  PaymentsServiceController,
+  PaymentsServiceControllerMethods,
+} from '@app/common';
 
 @Controller()
-export class PaymentsController {
+// By using this decorator, TS Proto will automatically add all the necessary
+// metadata to our methods
+@PaymentsServiceControllerMethods()
+export class PaymentsController implements PaymentsServiceController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
-  // Exposing the microservice functionality to other microservices
-  // This pattern is often associated with remote procedure call (RPC) style communication.
-  // When a microservice receives a message that matches the specified pattern, the associated method will be invoked to handle the message.
-  @MessagePattern('create_charge')
   @UsePipes(new ValidationPipe()) // Will automatically validate all the incoming data on this incoming payload => will validate using CreateChargeDto
-  async createCharge(
-    @Payload() data: PaymentsCreateChargeDto,
-    @Ctx() ctx: RmqContext,
-  ) {
-    // Getting the ack manually
-    const channel = ctx.getChannelRef();
-    const originalMessage = ctx.getMessage();
-
-    // It won't retry bc we are acking the message, it takes it off the queue
-    channel.ack(originalMessage);
-
+  async createCharge(data: PaymentsCreateChargeDto) {
     return this.paymentsService.createCharge(data);
   }
 }
