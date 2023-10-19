23529d101eb248c6dea8587bb96700c1dcf76a01
ArditZubaku <zubaku92@gmail.com>
2023-10-19 16:30:10 +0200

    Updated the Reservation entity to be compatible with GraphQL.

--- libs/common/src/database/abstract.entity.ts
+++ libs/common/src/database/abstract.entity.ts
@@ -1,7 +1,13 @@
+import { Field, ObjectType } from '@nestjs/graphql';
 import { PrimaryGeneratedColumn } from 'typeorm';
 
+@ObjectType({
+  // Type will not be registered in the schema, it will just be inherited by the other entity types
+  isAbstract: true,
+})
 export class AbstractEntity<T> {
   @PrimaryGeneratedColumn()
+  @Field()
   id: number;
 
   // It appends the passed entity properties to the existing one (it is basically id+abstractEntity)
