4977cb5b624d4449e961d5e7cb66963b86037423
ArditZubaku <zubaku92@gmail.com>
2023-10-20 16:36:32 +0200

    Refactored @CurrentUser decorator to be compatible with the authContext.

--- libs/common/src/decorators/current-user.decorator.ts
+++ libs/common/src/decorators/current-user.decorator.ts
@@ -2,7 +2,16 @@ import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 import { User } from '@app/common/models/user.entity';
 
 const getCurrentUserByContext = (context: ExecutionContext): User => {
-  return context.switchToHttp().getRequest().user;
+  if (context.getType() === 'http') {
+    return context.switchToHttp().getRequest().user;
+  }
+
+  // If context.getType() is GraphQL
+  const user = context.getArgs()[2]?.req.headers.user;
+  if (user) {
+    // Since we stringify the user in the gateway, we need to parse it back to an object
+    return JSON.parse(user);
+  }
 };
 
 export const CurrentUser = createParamDecorator(
